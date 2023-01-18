fe2083e6a35fbc77460c92ce0626aa5d8bb1177c
gozica <109421350+gozica@users.noreply.github.com>
2023-01-18 16:17:58 -0500

    generated win & lose

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
 userChoice = e.target.id
 userChoiceDisplay.innerHTML = userChoice
 generateComputerChoice()
+getResult()
 }))
 
 function generateComputerChoice() {
@@ -25,5 +27,31 @@ function generateComputerChoice() {
     if (randomNumber === 2) {
         computerChoice = 'paper'
     }
+    computerChoiceDisplay.innerHTML = computerChoice
 
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'it a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'you win!'
+    }
+     if (computerChoice === "rock" && userChoice === "scissors") {
+       result = "you lost!";
+     }
+      if (computerChoice === "paper" && userChoice === "scissors") {
+        result = "you win!";
+      }
+       if (computerChoice === "paper" && userChoice === "rock") {
+         result = "you lost!";
+       }
+        if (computerChoice === "scissors" && userChoice === "rock") {
+          result = "you win!";
+        }
+         if (computerChoice === "scissors" && userChoice === "paper") {
+           result = "you lost!";
+         }
+    
 }
