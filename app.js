e033c43a7d454df59837dec6930dfd94ac2d113f
gozica <109421350+gozica@users.noreply.github.com>
2023-01-18 16:19:20 -0500

    innerHTML result

--- app.js
+++ app.js
@@ -53,5 +53,7 @@ function getResult() {
          if (computerChoice === "scissors" && userChoice === "paper") {
            result = "you lost!";
          }
+
+         resultDisplay.innerHTML = result
     
 }
