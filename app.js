4f651419a185e01e9a40b0af372a389b131c84ed
gozica <109421350+gozica@users.noreply.github.com>
2023-01-18 16:10:58 -0500

    generated computer choice

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+userChoice = e.target.id
+userChoiceDisplay.innerHTML = userChoice
+generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor (Math.random() * possibleChoices.length)
+    
+    if (randomNumber === 0) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 1) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+
+}
